e84fbb52ec5a68eb2cf41f422b11a513cf52ac9b
Dmitry Krasavtsev <DmitryKrasavtsev1980u@gmail.com>
2025-09-21 21:44:57 +0700

    personal page tour

---
+++ resources/views/components/tour-url.blade.php
@@ -0,0 +1,38 @@
+<div class="max-w-7xl mx-auto p-2 sm:p-6">
+    <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:divide-y-0">
+        <div class="group relative rounded-tl-lg rounded-tr-lg border-gray-200 bg-white p-6 sm:rounded-tr-none sm:even:border-l">
+            <div class="mt-8">
+            <h3 class="text-base font-semibold text-gray-900">
+                <a href="https://neos-nsk.ru/tour/0dd21a29-aff0-43dd-9f7e-d617fe053f98" target="_blank" class="focus:outline-none">
+                <!-- Extend touch target to entire panel -->
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Виртуальный тур
+                </a>
+            </h3>
+            <p class="mt-2 text-sm text-gray-500">Просто полноэкранный виртуальный тур.</p>
+            </div>
+            <span aria-hidden="true" class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400">
+            <svg viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+            </svg>
+            </span>
+        </div>
+        <div class="group relative border-gray-200 bg-white p-6 sm:rounded-tr-lg sm:even:border-l">
+            <div class="mt-8">
+            <h3 class="text-base font-semibold text-gray-900">
+                <a href="https://neos-nsk.ru/tour/f1c7e62e-077e-4093-8c7e-9b19c6fc4657" target="_blank" class="focus:outline-none">
+                <!-- Extend touch target to entire panel -->
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Персональная страница
+                </a>
+            </h3>
+            <p class="mt-2 text-sm text-gray-500">Персональная страница для рекламы объекта недвижимости с картой, галереей фото, описанием и виртуальным туром.</p>
+            </div>
+            <span aria-hidden="true" class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400">
+            <svg viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+            </svg>
+            </span>
+        </div>
+    </div>
+</div>
